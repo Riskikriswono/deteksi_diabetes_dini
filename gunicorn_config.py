97a4b129bd8a76822dadc02a5121baea1ca3fb0a
Riski kriswono <riskikriswono@gmail.com>
2024-04-22 19:37:49 +0700

    update gunicorn_config

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,2 +1,16 @@
-bind ="0.0.0.0:8080"
-workers = 2
+# bind ="0.0.0.0:8080"
+# workers = 2
+
+# gunicorn_config.py
+
+# Bind address and port
+bind = '0.0.0.0:8000'
+
+# Number of worker processes
+workers = 3
+
+# Worker class for handling requests
+worker_class = 'sync'
+
+# Timeout for worker processes
+timeout = 30
